b75e56fe7709104d147f355df374ce4bfb7ea4ca
sallykorea <khk5087@gmail.com>
2020-01-18 23:18:20 +0900

    todo list 입력, 작성한 내용 불러오기 완성

--- todo.js
+++ todo.js
@@ -10,19 +10,46 @@ const toDoForm=document.querySelector(".js-toDoForm"),
 
 const TODOS_LS="toDos";
 
+const toDos=[];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li=document.createElement("li");
+    const delBtn=document.createElement("button");
+    const span=document.createElement("span");
+    const newId=toDos.length+1;
+    delBtn.value=String.fromCodePoint(0x274C);
+    span.innerText=text
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id=newId;
+    toDoList.appendChild(li);
+    const toDoObj={
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
 function handleSubmit(event){
-    console.log('dd');
     event.preventDefault();
     const currentValue=toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value="";
 }
 
 function loadToDos(){
-    const toDos=localStorage.getItem(TODOS_LS);
-    if(toDos!==null){
-        
+    const loadedToDos=localStorage.getItem(TODOS_LS);
+    if(loadedToDos!==null){
+        const parsedToDos=JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     }
-    
 }
 
 function init(){
