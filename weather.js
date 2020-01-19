6a820751c4fb22c263642fa3822885bf85277ad9
sallykorea <khk5087@gmail.com>
2020-01-19 20:22:55 +0900

    api로 날씨 데이터 갖고 와서 출력하기

--- weather.js
+++ weather.js
@@ -1,4 +1,18 @@
 const COORDS="coords";
+const API_KEY="955114380d6bdff16c69f9eb73ec633d";
+const weather=document.querySelector(".js-weather");
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature=json.main.temp;
+        const place=json.name;
+        weather.innerText=`${temperature}@${place}`
+    });
+}
 
 function handleGeoError(){
     console.log("cant access");
@@ -16,6 +30,7 @@ function handleGeoSucces(position){
         longitude:longitude
     }
     saveCoord(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function askForCoords(){
@@ -27,7 +42,9 @@ function loadCoords(){
     if(loadCoords===null){
         askForCoords();
     }else{
-        
+        const parseCoords=JSON.parse(loadCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
     }
 }
 
